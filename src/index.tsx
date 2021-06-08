1ca9606f6bad4df5a851a5d816749b1e80c7e25d
Vincent Onyango <verencelola@icloud.com>
2021-06-08 20:52:41 +0300

    - project setup  - add bootstrap, prop-types and eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from "react-dom";
+import React from "react";
 
-import App from "./App";
+import App from "./App/App";
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);

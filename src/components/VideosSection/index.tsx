757061b865da96dbc60e131aeca3754ab378e7a0
Vincent Onyango <verencelola@icloud.com>
2021-06-10 17:22:24 +0300

    - finish styling the home page

--- src/components/VideosSection/index.tsx
+++ src/components/VideosSection/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 import { useHistory } from "react-router";
 
-import "./index.css";
+import "./VideoSection.scss";
 
 const VideosSection = (props: VideosSection) : ReactElement => {
 	const {videos = [], title} = props;
@@ -12,12 +12,12 @@ const VideosSection = (props: VideosSection) : ReactElement => {
 	};
 
 	return (
-		<div className='d-flex flex-column'>
-			<div className='text-start text-capitalize'>
-				<p className='h6 mt-2' style={{color: "white"}}>{title}</p>
+		<div className='category-container'>
+			<div className='category-name'>
+				<p>{title}</p>
 			</div>
-			<div className='d-flex overflow-auto px-2 mx-2'>
-				{videos.map(({muxPlaybackID, id}) => <img role='button' onClick={() => onClick(id)} key={id} className='video-thumbnail me-3 pt-2' width='200' height='200' src={`https://image.mux.com/${muxPlaybackID}/thumbnail.jpg`} />)}
+			<div className='videos'>
+				{videos.map(({muxPlaybackID, id}) => <img role='button' onClick={() => onClick(id)} key={id} width='200' height='200' src={`https://image.mux.com/${muxPlaybackID}/thumbnail.jpg`} />)}
 			</div>
 		</div>
 	);

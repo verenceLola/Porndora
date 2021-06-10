aa9ffdb16fbf0dbe8cd99651a259d4396952c3f6
Vincent Onyango <verencelola@icloud.com>
2021-06-10 16:37:28 +0300

    - fix footer styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,14 +9,14 @@ import NewVideos from "../../components/NewVideos";
 const Home = () : ReactElement => (
 	<div className="App d-flex flex-column h-100 bg-dark">
 		<Header loggedIn={false}/>
-		<main className='flex-shrink-0'>
+		{/* <main className='flex-shrink-0'>
 			<div style={{height: "500px"}} className='mx-0 px-0 w-100'>
 				<div className='overflow-auto h-100 w-100'>
 					<PopularVideos />
 					<NewVideos />
 				</div>
 			</div>
-		</main>
+		</main> */}
 		<Footer />
 	</div>
 );
